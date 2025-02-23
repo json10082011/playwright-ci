7d8720ff24c9c795c968c2d12949714d0ea18334
json10082011 <143684836+json10082011@users.noreply.github.com>
2025-02-23 15:36:30 +0800

    Update the assertion

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,5 +2,5 @@ import { test, expect } from '@playwright/test';
 
 test('Check Playwright homepage', async ({ page }) => {
     await page.goto('https://facebook.com/');
-    await expect(page).toHaveTitle(/Facebook/);
+    await expect(page).toHaveTitle(/Facebooks/);
 });
