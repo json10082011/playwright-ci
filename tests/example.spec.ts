3eab0cb68ff0b36f8ccac2fecfa2b67598dfddfe
json10082011 <json10082011@gmail.com>
2025-02-23 07:11:32 +0000

    Initial commit

---
+++ tests/example.spec.ts
@@ -0,0 +1,6 @@
+import { test, expect } from '@playwright/test';
+
+test('Check Playwright homepage', async ({ page }) => {
+    await page.goto('https://facebook.com/');
+    await expect(page).toHaveTitle(/Facebook/);
+});
